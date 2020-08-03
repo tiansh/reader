fb7e2b402e643e9dfe886a20bac55be45dc32ee0
田生 <tiansheng111@gmail.com>
2020-08-03 23:04:04 +1000

    page

--- sw.js
+++ sw.js
@@ -1,4 +1,13 @@
-const version = '20200725';
+/*!
+ * @license MPL-2.0-no-copyleft-exception
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * This Source Code Form is "Incompatible With Secondary Licenses", as
+ * defined by the Mozilla Public License, v. 2.0.
+ */
+
+const version = '20200803';
 
 const resourceList = [
   './css/normalize-8.0.1.css',
