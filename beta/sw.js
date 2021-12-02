48653ac5cce3fc0545c84dca5146eb577c8d697e
田生 <tiansheng111@gmail.com>
2021-12-02 23:00:18 +1100

    page beta

--- beta/sw.js
+++ beta/sw.js
@@ -7,7 +7,7 @@
  * defined by the Mozilla Public License, v. 2.0.
  */
 
-const version = /* VERSION */"20211130"/* VERSION */; // eslint-disable-line quotes
+const version = /* VERSION */"20211202"/* VERSION */; // eslint-disable-line quotes
 
 const resourceList = [
   './help/about.html',
